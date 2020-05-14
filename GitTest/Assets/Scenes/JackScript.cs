1fb9b3a18083065d9d496907d7925bc5c3d4334e
Rob <rromanomusic@gmail.com>
2020-05-14 16:43:52 +1000

    New Script
    Added a new script

---
+++ GitTest/Assets/Scenes/JackScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Yeeeeeeh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
